ff0c392b45f0722a3441464317537b750dd0bb0c
brayan20021 <brayanrodriguez_007@hotmail.com>
2023-04-13 15:39:08 -0400

    error fixed and eventlogic added

---
+++ js/messages.js
@@ -0,0 +1,34 @@
+const conversacionMadre = {
+  
+parrafo1: [
+"¡Buenos días, mi querido hijo! ¡Hoy es un día muy importante! ¿Estás listo para comenzar tu aventura en el mundo Pokémon?",
+"Antes de que te vayas, asegúrate de que tienes todo lo necesario para tu aventura. Revisa tu bolsa y tu Pokédex.",
+"¡Buena suerte en tu viaje, hijo! Espero que disfrutes de esta nueva aventura y te diviertas mucho. ¡Te quiero!" 
+]
+
+};
+
+const oak_conversation = {
+
+parrafo1: [
+
+"¡Bienvenido/a! ¿Estás listo/a para empezar tu aventura en el mundo de Pokémon?",
+"Antes de que comiences, déjame decirte que necesitas un compañero para acompañarte en tu viaje.",
+"Puedes escoger entre Bulbasaur, Charmander y Squirtle. ¿Cuál te gustaría tener como tu primer Pokémon?",
+"Debes escoger tu pokemon en la mesa"
+
+]
+
+};
+
+const systemNotification = {
+
+parrafo1: [
+
+"¡Hey espera! Ciudad Paleta necesita saber tu nombre",
+"Un pokemon te ha encontrado!, tienes que luchar para defenderte",
+"Puedes escoger entre Bulbasaur, Charmander y Squirtle. ¿Cuál te gustaría tener como tu primer Pokémon?",
+
+]
+    
+};
