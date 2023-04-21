7cf2214fa7e53dbc2e021b6414e358a6cedb9a92
brayan20021 <brayanrodriguez_007@hotmail.com>
2023-04-21 16:56:46 -0400

    added city battle and bugs fixed

--- js/messages.js
+++ js/messages.js
@@ -3,7 +3,9 @@ const conversacionMadre = {
 parrafo1: [
 "¡Buenos días, mi querido hijo! ¡Hoy es un día muy importante! ¿Estás listo para comenzar tu aventura en el mundo Pokémon?",
 "Antes de que te vayas, asegúrate de que tienes todo lo necesario para tu aventura. Revisa tu bolsa y tu Pokédex.",
-"¡Buena suerte en tu viaje, hijo! Espero que disfrutes de esta nueva aventura y te diviertas mucho. ¡Te quiero!" 
+"Hijo el campo esta en el norte esta es la direccion que me dieron X: -50 Y: -165 dirigete a esas coordenadas",
+"¡Buena suerte en tu viaje, hijo! Espero que disfrutes de esta nueva aventura y te diviertas mucho. ¡Te quiero!",
+
 ]
 
 };
@@ -28,7 +30,7 @@ parrafo1: [
 "¡Hey espera! Ciudad Paleta necesita saber tu nombre",
 "Un pokemon te ha encontrado!, tienes que luchar para defenderte",
 "Puedes escoger entre Bulbasaur, Charmander y Squirtle. ¿Cuál te gustaría tener como tu primer Pokémon?",
-"Felicidades!, has ganado la pelea, pero tu pokemon necesita asistencia medica"
+"Felicidades!, has ganado la pelea, te has destacado en la ciudad y has sorprendido a todos, ahora los rivales se esforzaran mas y estaran pendiente a ti, dirigete a la coordenada X: -540 Y: -210 te ayudaran a fortalecer tus habilidades"
 
 ]
     
@@ -37,7 +39,7 @@ parrafo1: [
 const nps_city = {
 
     parrafo1: [
-        "Hola, como estas a donde vas",
+        "Bienvenido a la ciudad, la ciudad esta vacia, todos/as estan en la arena viendo los mejores torneos de pokemon",
         "He escuchado que en esta ciudad, hacen las mejores peleas de pokemon,",
         "Si deseas participar te recomiendo que vayas a la arena que queda en el norte de la ciudad"
     ]
